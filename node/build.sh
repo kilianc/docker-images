f3982e8a9576576dc5c9776ca604558f8661cb29
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2015-12-03 00:53:17 -0800

    init

---
+++ node/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -e
+
+VERSIONS=(
+  0.12.7
+  0.12.8
+  4.0.0
+  4.1.0
+  4.1.1
+  4.1.2
+  4.2.1
+  4.2.2
+  5.0.0
+  5.1.0
+)
+
+function build_image {
+  local dockerfile_path="$1"
+  local base_image_name="$2"
+  local image_name="$2:$3"
+  local node_version_in="$4"
+
+  local dockerfile=$(cat "$1" | sed "s/__VERSION__/$node_version_in/g")
+  local build_output=$(echo "$dockerfile" | docker build -t "$image_name" -)
+  local node_version_out=$(docker run --rm "$image_name" node -v)
+
+  if [[ "$node_version_out" != "v$node_version_in" ]]; then
+    echo " > error node_version=$node_version_out expected v$node_version_in"
+    echo "$build_output"
+    return 1
+  fi
+
+  echo " > $image_name -> $node_version_out"
+}
+
+function build_images {
+  local dockerfile_path="$1"
+  local base_image_name="$2"
+
+  for version in "${VERSIONS[@]}"; do
+    build_image "$dockerfile_path" "$base_image_name" "$version" "$version"
+  done
+
+  build_image "$dockerfile_path" "$base_image_name" "latest" "$version"
+}
+
+function push {
+  echo " > pushing to https://hub.docker.com/r/$1/tags/"
+  docker push "$1"
+}
+
+base_image_name="$(echo "kilianciuffolo/$1" | sed "s/dockerfile-//")"
+build_images "$1" "$base_image_name"
+push "$base_image_name"
